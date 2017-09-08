8ac49fac2a3e806709924dbc5ac46a20f51064c6
tanqilin <tanqilin@liweijia.com>
2017-09-08 15:47:31 +0800

    滑动js默认选中bug修护

--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -1,20 +1,25 @@
-!(function(win, lwjui) {
+/** lwj-v MIT License By  */
+ !(function(win, lwjui) {
 	"use strict";
 	lwjui.swiper = (function($W) {
 		$W.init = function(el, config, dir) {
 			this.el = el;
 			this.scope = config;
-			this.scope.config =  this.scope.config ? this.scope.config : {
+			this.scope.config = this.scope.config ? this.scope.config : {
 				pagination: '.swiper-pagination',
-        paginationClickable: '.swiper-pagination',
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev'
-			};  
+				paginationClickable: '.swiper-pagination',
+				nextButton: '.swiper-button-next',
+				prevButton: '.swiper-button-prev'
+			};
 			this.dir = dir;
+			this.scope.config.onInit = function(swiper){
+				$(el).addClass('opacity1');
+			};
 			return this.service();
 		};
 		$W.service = function() {
-			return new Swiper(this.el,this.scope.config)
+			var swiper = new Swiper(this.el, this.scope.config);
+			return swiper;
 		}
 		return $W;
 	})({});
@@ -30,16 +35,19 @@
 			addcss: ['plugin/swiper/swiper.css'],
 			scope: {
 				config: '=config',
-				callback:'~complete'
+				callback: '~complete'
 			},
 			link: function(el) {
 				var scope = el.scope,
 					service = el.fn.swiper;
 				// 扩展回调方法
 				var service = service.init(el.element, scope, el.fn.cache.dir + "plugin/swiper/");
-				if(scope.callback){
+				if(typeof scope.config.initialSlide != "undefined" ){
+					$(el.element).find(".swiper-slide:eq(" + (scope.config.initialSlide || 0) + ")").addClass('active');
+				}
+				if (scope.callback) {
 					scope.callback(service)
-				}				
+				}
 			}
 		};
 	});
